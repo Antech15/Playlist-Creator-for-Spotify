a5c92a69279bd51839476b0ad400575f3759a0da
antech15 <shakycrab@gmail.com>
2023-04-10 15:44:54 -0700

    djjkrnf

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
+#For .env
+from dotenv import load_dotenv
+import os
+
+import base64
 from flask import Flask, redirect, request, jsonify, render_template
 import requests
+import json
 
 app = Flask(__name__)
+
+#credentials for Spotify API
+CLIENT_ID = os.getenv("CLIENT_ID")
+
 
 @app.route("/")
 def index():
